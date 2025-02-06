1a0ba80d8b37fd07016adbf2256b653c33dd038c
Clayton Toney <Toneyclayton@gmail.com>
2025-02-06 14:57:49 -0500

    feat: :sparkles: added new page for comments

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/cars', [CarController::class, 'index'])->name('car');
-Route::get('/about', [AboutController::class, 'about'])->name('about');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+Route::get('/comments/{id}', [CommentController::class, 'show'])->name('comments.show');
+Route::get('/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/comments/{id}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
